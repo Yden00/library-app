8b8c454150e28c9cc5fc3682c0419313016e2abe
ue4prog <vladtett2000@gmail.com>
2023-08-01 20:41:33 +0200

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import styles from '../style';
-import NavBar from './NavBar.tsx';
+import { navLinks } from '../constants';
 
 const Header = () => {
   const [burgerActive, setBurgerActive] = useState('')
@@ -8,10 +8,13 @@ const Header = () => {
     <nav className={styles.nav}>
       <h1 className={`${styles.heading1}`}>Brooklyn Public Library</h1>
       <div className='flex ml-auto'>
-        <NavBar 
-          ulClassName={`list-none md:flex hidden justify-between items-center w-[90%] pt-1`} 
-          liClassName={`font-inter ml-[30px] font-regular cursor-pointer text-header_footer_text text-[1.27rem] ${styles.hover} leading-10 ${ styles.text }`}
-        />
+        <ul className={`list-none md:flex hidden justify-between items-center w-[90%] pt-1`} >
+            {navLinks.map((nav) => (
+              <li key={nav.id} className={`font-inter ml-[30px] font-regular cursor-pointer text-header_footer_text text-[1.27rem] ${styles.hover} leading-10 ${ styles.text }`}>
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+        </ul>
         <a href="#"><img src='/icon_profile.png' alt="Profile" className='icon-profile xs:block hidden'/></a>
         <div className='burger-img md:hidden block mt-1 xxs:ml-[20px] ml-[32px]' onClick={() => burgerActive ? setBurgerActive('') : setBurgerActive('active')}>
             <span className={`line ${burgerActive}`}></span>
@@ -20,10 +23,13 @@ const Header = () => {
         </div>
         <div className={`burger-menu xs:pt-[22px] pt-[27px] pb-[70px] extras:w-[332px] w-full relative ${burgerActive}`}>
           <a href="#"><img src='/icon_profile.png' alt="Profile" className='icon-profile'/></a>
-          <NavBar 
-            ulClassName={`list-none md:hidden flex flex-col justify-between items-center `} 
-            liClassName={`font-inter pb-[30px] ml-[30px] font-regular cursor-pointer text-header_footer_text text-[1.27rem] ${styles.hover} leading-10 ${ styles.text }`}
-          />
+          <ul className={`list-none md:hidden flex flex-col justify-between items-center `} >
+            {navLinks.map((nav) => (
+              <li key={nav.id} className={`font-inter pb-[30px] ml-[30px] font-regular cursor-pointer text-header_footer_text text-[1.27rem] ${styles.hover} leading-10 ${ styles.text }`}>
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
         </div>
         <div>
         </div>
@@ -31,6 +37,8 @@ const Header = () => {
     </nav>
   )
 }
+
+
 
 
 export default Header
